56910d91dd7f94d6a25c55b555267c4ffbd09c68
sven <nguyenhongky1998@gmail.com>
2021-07-06 08:35:41 +0700

    base config

--- vm.sh
+++ vm.sh
@@ -6,39 +6,39 @@ read -p "The Hostname of this VM: " host_name
 read -p "The Designated IP Address of this VM: " ip
 defaultpass="Aa@123456"
 
-local_config () {
+local_config() {
 	sudo useradd -m $username -s /bin/bash
 	sudo echo $defaultpass | passwd $username --stdin
 	sudo mkdir -p /home/$username/.ssh
-	sudo usermod -aG $group $username 
-	sudo echo $user_public_key > /home/$username/.ssh/authorized_keys
+	sudo usermod -aG $group $username
+	sudo echo $user_public_key >/home/$username/.ssh/authorized_keys
 	sudo chown -R $username:$username /home/$username/.ssh
 	sudo chmod 644 /home/$username/.ssh/authorized_keys
 	sudo sed "s/template/$host_name/g" -i /etc/hostname
 	sudo sed "s/PasswordAuthentication yes/PasswordAuthentication no/g" -i /etc/ssh/sshd_config
 }
-source /etc/os-release
-read "Add new user? [yes(y)/no(n)] " answer
-while [[ $answer == "y"  $answer == "yes" ]]
-do
-	if [[ $ID == "debian" || $ID == "ubuntu" ]]
-	then
-		echo "Detected $PRETTY_NAME which is supported"
-		group_name="sudo"
-		local_config
-		sudo sed "s/172.16.200.12/$ip/g" -i /etc/netplan/oo-installer-config.yaml
-		sudo netplan apply
-	elif [[ $ID == "centos" || $ID == "rhel"  ]]
-	then
-		echo "Detected $PRETTY_NAME which is supported"
-		group_name="wheel"
-		local_config
-		sudo sed "s/172.16.200.12/$ip/g" -i /etc/sysconfig/network-scripts/ifcfg-eth0
-		sudo systemctl restart network
-	else
-		echo "Unsupported Distribution"
-	fi
-done
+yum() {
+	echo "Detected $PRETTY_NAME which is supported"
+	group_name="sudo"
+	local_config
+	sudo sed "s/172.16.200.12/$ip/g" -i /etc/netplan/oo-installer-config.yaml
+	sudo netplan apply
+}
+apt() {
 
-sudo systemctl restart sshd 
+	echo "Detected $PRETTY_NAME which is supported"
+	group_name="wheel"
+	local_config
+	sudo sed "s/172.16.200.12/$ip/g" -i /etc/sysconfig/network-scripts/ifcfg-eth0
+	sudo systemctl restart network
+}
+source /etc/os-release
+if [[ $ID == "debian" || $ID == "ubuntu" ]]; then
+	yum
+elif [[ $ID == "centos" || $ID == "rhel" ]]; then
+	apt
+else
+	echo "Unsupported Distribution"
+fi
+sudo systemctl restart sshd
 sudo systemctl reboot now
